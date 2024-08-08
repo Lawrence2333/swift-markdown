ea304bcb8a6a393cdae40a08793b74896dc1f438
lawrence lin <lawrence@msh.team>
2024-08-08 16:23:11 +0800

    fix(math): math plain text should be \( \)

--- Sources/Markdown/Inline Nodes/Inline Containers/Math.swift
+++ Sources/Markdown/Inline Nodes/Inline Containers/Math.swift
@@ -38,7 +38,7 @@ public extension Math {
         let childrenPlainText = children.compactMap {
             return ($0 as? InlineMarkup)?.plainText
         }.joined()
-        return "[^\(childrenPlainText)^]"
+        return "\\(\(childrenPlainText)\\)"
     }
 
     // MARK: Visitation
