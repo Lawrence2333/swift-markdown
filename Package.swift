59a004929a42f268353075493d04e8182685d365
lawrence lin <lawrence@msh.team>
2024-10-11 22:35:49 +0800

    temp: change branch while in develop

--- Package.swift
+++ Package.swift
@@ -46,7 +46,9 @@ let package = Package(
 if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     // Building standalone, so fetch all dependencies remotely.
     package.dependencies += [
-        .package(url: "https://github.com/Arthraim/cmark-mfm.git", branch: "mfm"),
+//        .package(url: "https://github.com/Arthraim/cmark-mfm.git", branch: "mfm"),
+        .package(url: "https://github.com/Lawrence2333/cmark-mfm.git", branch: "feature/answer_support_images"),
+        // .package(path: "~/Desktop/codes/markdown/cmark-mfm")
     ]
     
     // SwiftPM command plugins are only supported by Swift version 5.6 and later.
